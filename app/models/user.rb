387827c2e331d435070b6edf49d21f2245ed5ef2
zare926 <ilove.boc926@gmail.com>
2020-07-10 11:09:53 +0900

    画像配置、背景色設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  devise :validatable, password_length: 8..128
 end
