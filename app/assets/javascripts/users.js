065719a71a931933a3acc5c1925465f2dbe42d7e
zare926 <ilove.boc926@gmail.com>
2020-07-11 00:58:26 +0900

    アイコン画像プレビュー

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,4 +6,26 @@ $(function(){
   $(fakeBtn).on('mousedown',function(){
     $(realBtn).click();
   });
+
+  //Icon画像のプレビュー
+  $('.icon_image-form').change(function(e){
+    //ファイルオブジェクトを取得する
+    let file = e.target.files[0];
+    let reader = new FileReader();
+ 
+    //画像でない場合は処理終了
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。");
+      return false;
+    }
+ 
+    //アップロードした画像を設定する
+    reader.onload = (function(file){
+      return function(e){
+        $(".icon_image").attr("src", e.target.result);
+        $(".icon_image").attr("title", file.name);
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
 });
