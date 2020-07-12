1145cee790c8975dee46e2c8688856eaabc159cc
zare926 <ilove.boc926@gmail.com>
2020-07-13 07:01:42 +0900

    html作成、ルーティングとコントローラー記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
   def index
     # @posts = Posts.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+  end
+
 end
