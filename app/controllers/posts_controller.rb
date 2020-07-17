18dabea4f84b4e16660dd65ea4e4bb88023f393a
zare926 <ilove.boc926@gmail.com>
2020-07-18 00:58:42 +0900

    User_idをmergeできるように

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
 
   private 
   def post_params
-    params.require(:post).permit(:title,:text,:image)
+    params.require(:post).permit(:title,:text,:image).merge(user_id: current_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_122525) do
+ActiveRecord::Schema.define(version: 2020_07_17_155333) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_122525) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
