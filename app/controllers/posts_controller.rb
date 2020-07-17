e096547699d1c57f85944e250066919329dcc7e9
zare926 <ilove.boc926@gmail.com>
2020-07-18 00:53:47 +0900

    postsテーブルにuser_id追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,16 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    post = Post.new(post_params)
+    if post.save
+      redirect_to root_path and return
+    else
+      redirect_to new_post_path and return
+    end
   end
 
+  private 
+  def post_params
+    params.require(:post).permit(:title,:text,:image)
+  end
 end
