8d97c737863803915a2fecd31c5f66094e4c0d8b
zare926 <ilove.boc926@gmail.com>
2020-07-20 00:18:29 +0900

    記事の画像、サイズ設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [1440, 900, "#fff", "Center"]
+  process resize_to_fill: [850, 525, "Center"]
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
     storage :file
