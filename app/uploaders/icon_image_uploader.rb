5d7a50134216c5e96f2539c6e1f73af6559f183e
zare926 <ilove.boc926@gmail.com>
2020-07-20 00:23:11 +0900

    Iconサイズ変更、turbolinks追記

--- app/uploaders/icon_image_uploader.rb
+++ app/uploaders/icon_image_uploader.rb
@@ -2,7 +2,7 @@ class IconImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
+  process resize_to_fill: [200, 200, "Center"]
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
     storage :file
