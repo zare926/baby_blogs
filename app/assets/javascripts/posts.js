2c2c04093094ccd0d2b68a78b7fea57d299f499e
zare926 <ilove.boc926@gmail.com>
2020-07-24 06:32:22 +0900

    スクロールしてfadein

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -113,3 +113,26 @@ $(function(){
     reader.readAsDataURL(file);
   });
 });
+
+// scroll後fadein
+$(function(){
+  function animation(){
+    $('.fadein').each(function(){
+      //ターゲットの位置を取得
+      let target = $(this).offset().top;
+      //スクロール量を取得
+      let scroll = $(window).scrollTop();
+      //ウィンドウの高さを取得
+      let windowHeight = $(window).height();
+      //ターゲットまでスクロールするとフェードインする
+      if (scroll > target - windowHeight){
+        $(this).css('opacity','1');
+        $(this).css('transform','translateY(0)');
+      }
+    });
+  }
+  animation();
+  $(window).scroll(function (){
+    animation();
+  });
+});
