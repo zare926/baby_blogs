6fc569f8e40360d5d62fbc9b9528f84e8154071a
zare926 <ilove.boc926@gmail.com>
2020-08-01 22:15:50 +0900

    hover時のcss

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_params, only:[:show]
   before_action :time, only:[:index, :show]
-  PER = 5
+  PER = 7
 
   def index
     @posts = Post.all.order("created_at DESC").page(params[:page]).per(PER)
