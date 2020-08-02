4a9cb9f6b6838e0ab2bbbbcf6af64c73e4f0f403
zare926 <ilove.boc926@gmail.com>
2020-08-03 06:43:15 +0900

    デプロイ設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload secrets.yml'
+  desc 'upload credentials.yml.enc'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
