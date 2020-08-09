ea1146246cc80758ccf1d11d028bd402a44dc7a3
zare926 <ilove.boc926@gmail.com>
2020-08-09 19:53:48 +0900

    redirect系1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_params, only:[:show]
   before_action :time, only:[:index, :show,:new]
+  before_action :move_to_new, except: [:index,:show]
   PER = 7
 
   def index
@@ -36,6 +37,10 @@ class PostsController < ApplicationController
 
   def time
     @d = Date.today
+  end
+
+  def move_to_new
+    redirect_to action: :index unless user_signed_in?
   end
   
 end
