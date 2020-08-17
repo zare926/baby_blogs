3b5fee538366d8ec735e1c079b5024d63b26b90c
zare926 <ilove.boc926@gmail.com>
2020-08-18 00:45:53 +0900

    背景選択切り替え

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -7,4 +7,26 @@ $(document).on('turbolinks:load', function(){
       $('.edit_box').addClass('clear_box');
       return false;
   });
+  
+  $('[name=bgi').change(function() {
+
+  let bgi = $(this).val();
+  let Img = $('.user_show-header').attr('src');
+  let nowImg = 'img[src="' + Img + '"]';
+    if (bgi == "背景1") {
+      $(nowImg).attr('src','/assets/show_header1.jpg');
+    } else if (bgi == "背景2") {
+      $(nowImg).attr('src','/assets/show_header2.jpg');
+    } else if (bgi == "背景3") {
+      $(nowImg).attr('src','/assets/show_header3.jpg');
+    } else if (bgi == "背景4") {
+      $(nowImg).attr('src','/assets/show_header4.jpg');
+    } else if (bgi == "背景5") {
+      $(nowImg).attr('src','/assets/show_header5.jpg');
+    } else if (bgi == "背景6") {
+      $(nowImg).attr('src','/assets/show_header6.jpg');
+    } else if (bgi == "背景7") {
+      $(nowImg).attr('src','/assets/show_header7.jpg');
+    }
+    });
 });
