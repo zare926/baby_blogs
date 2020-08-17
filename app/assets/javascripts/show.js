e7b0f06de278a724e6ceb808a9ad347230973fa7
zare926 <ilove.boc926@gmail.com>
2020-08-17 22:46:13 +0900

    user削除

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,12 +1,10 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
   $('.fa-bars').on('click',function(){
       $('.edit_box').removeClass('clear_box');
-      $('.edit_box-image').css('display','');
       return false;
   });
   $('.close_btn').on('click',function(){
       $('.edit_box').addClass('clear_box');
-      $('.edit_box-image').css('display','none');
       return false;
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :time, only:[:index, :show,:new]
+  before_action :set_params, only:[:show,:edit,:destroy]
   PER = 7
 
   def new
@@ -16,7 +17,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @item = @user.posts
     @items = @item.order("created_at DESC").page(params[:page]).per(PER)
   end
@@ -30,12 +30,20 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    
+    if @user.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
   def user_params
     params.require(:user).premit(:nickname,:icon,:email,:encrypted_password)
+  end
+
+  def set_params
+    @user = User.find(params[:id])
   end
 
   def time
